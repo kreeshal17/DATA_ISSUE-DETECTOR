3b719f10fe37c1e906e7da17f67a8f21cc978d32
kreeshal17 <karnakreeshal@gmail.com>
2026-08-13 09:41:26 +0530

    add dataset AI analysis endpoint

--- backend/datasets/urls.py
+++ backend/datasets/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import DatasetView
+from .views import DatasetView,AnalyzeDatasetView
 
 
 urlpatterns = [
     path("", DatasetView.as_view(), name="datasets"),
+    path(
+    "<slug:slug>/analyze/",
+    AnalyzeDatasetView.as_view(),
+    name="analyze-dataset"
+),
 ]
--- backend/datasets/views.py
+++ backend/datasets/views.py
@@ -2,10 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+
+from issues.models import Issue
 from issues.services import IssueDetector
 from .models import Dataset
 from .serializers import DatasetSerializer
 from profiling.services import ProfilerService
+from ai_analysis.graph import graph
 
 
 class DatasetView(APIView):
@@ -49,3 +52,58 @@ class DatasetView(APIView):
         )
 
         return Response(serializer.data)
+
+
+class AnalyzeDatasetView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, slug):
+
+        try:
+            dataset = Dataset.objects.get(
+                owner=request.user,
+                slug=slug
+            )
+
+        except Dataset.DoesNotExist:
+            return Response(
+                {"error": "Dataset not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        issues = Issue.objects.filter(
+            dataset=dataset
+        )
+
+        if not issues.exists():
+            return Response(
+                {"error": "No issues found for this dataset"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        analyzed = []
+
+        for issue in issues:
+
+            graph.invoke({
+                "issue_id": issue.id,
+                "issue": "",
+                "analysis": None
+            })
+
+            issue.refresh_from_db()
+
+            analyzed.append({
+                "issue_id": issue.id,
+                "ai_explanation": issue.ai_explanation,
+                "ai_root_cause": issue.ai_root_cause,
+                "ai_recommendation": issue.ai_recommendation,
+                "ai_confidence": issue.ai_confidence,
+            })
+
+        return Response({
+            "dataset": dataset.name,
+            "analyzed_count": len(analyzed),
+            "issues": analyzed
+        })
